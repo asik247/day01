cc9e9a69bb98daa34bc136574176d7aeb039a738
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-03 01:25:39 +0600

    const

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let name ="Asik"
-let age = 20;
-let height = 5.7;
+// let name ="Asik"
+// let age = 20;
+// let height = 5.7;
+// name = "rimon"
+// age = 25;
+// height =6
+// console.log(name+age+height);
+const name ="Asik"
+const age = 20;
+const height = 5.7;
 name = "rimon"
 age = 25;
 height =6
@@ -9,4 +16,5 @@ height =6
 console.log(name+age+height);
 /**
  * Redeclare kora jai na but value update kore jai (let)
+ * Redeclare kora jai na and value update kora oo jai na tobe object/array ar vetore a value change kore jai (const)
  */
