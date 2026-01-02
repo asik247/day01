a03c7042a2ca0185a2e386e0653e9fb39f441b9d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-03 01:35:00 +0600

    const done

--- script.js
+++ script.js
@@ -5,16 +5,29 @@
 // age = 25;
 // height =6
 // console.log(name+age+height);
-const name ="Asik"
-const age = 20;
-const height = 5.7;
-name = "rimon"
-age = 25;
-height =6
+// const name ="Asik"
+// const age = 20;
+// const height = 5.7;
+// name = "rimon"
+// age = 25;
+// height =6
 
 
-console.log(name+age+height);
+// console.log(name+age+height);
+
+// Object in const
+const students ={
+name:'ask',
+age:20
+}
+students.age = 50
+console.log(students);
+// Array in const
+const firends = ['asik','rimon','karim']
+firends[0]="kahan"
+console.log(firends);
+
 /**
  * Redeclare kora jai na but value update kore jai (let)
- * Redeclare kora jai na and value update kora oo jai na tobe object/array ar vetore a value change kore jai (const)
+ * Redeclare kora jai na and value update kora oo jai na tobe object/array ar vetore ar value change kore jai (const)
  */
